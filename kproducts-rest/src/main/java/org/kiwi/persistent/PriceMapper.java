162b3cf7fb15dbc4246752c86bcca53e7b277dbc
kiwi <jcwen@thoughtworks.com>
2014-06-25 11:56:13 +0800

    get all prices

--- kproducts-rest/src/main/java/org/kiwi/persistent/PriceMapper.java
+++ kproducts-rest/src/main/java/org/kiwi/persistent/PriceMapper.java
@@ -7,9 +7,10 @@ import org.kiwi.domain.Product;
 import java.util.List;
 
 public interface PriceMapper {
-    public List<Price> getProductPrices(Product productId);
+    List<Price> getProductPrices(@Param("product")Product productId);
 
     Price getPrice(@Param("product") Product product, @Param("priceId") int priceId);
 
     int createPrice(@Param("product") Product product, @Param("price") Price price);
+
 }
--- kproducts-rest/src/test/java/org/kiwi/persistent/PriceMapperTest.java
+++ kproducts-rest/src/test/java/org/kiwi/persistent/PriceMapperTest.java
@@ -8,6 +8,7 @@ import org.kiwi.domain.Price;
 import org.kiwi.domain.Product;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -32,7 +33,9 @@ public class PriceMapperTest {
         appleJuice = new Product("apple juice", "good");
         productMapper.createProduct(appleJuice);
         appleJuicePrice = new Price(120, "kiwi", new Timestamp(114, 1, 1, 0, 0, 0, 0));
+        Price appleJuicePrice2 = new Price(150, "kiwi", new Timestamp(113, 1, 1, 0, 0, 0, 0));
         priceMapper.createPrice(appleJuice, appleJuicePrice);
+        priceMapper.createPrice(appleJuice, appleJuicePrice2);
     }
 
     @After
@@ -47,5 +50,12 @@ public class PriceMapperTest {
         final Price price = priceMapper.getPrice(appleJuice, product.getCurrentPrice().getId());
 
         assertThat(price.getPrice(), is(120));
+    }
+
+    @Test
+    public void should_get_all_prices() {
+        final List<Price> productPrices = priceMapper.getProductPrices(appleJuice);
+
+        assertThat(productPrices.size(), is(2));
     }
 }
